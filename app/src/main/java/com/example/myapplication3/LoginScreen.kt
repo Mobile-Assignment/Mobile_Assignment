b439220751d0b0f7d731270f9e6e350c5ba74a74
Desmond ( Dessy ) <d3smond95@gmail.com>
2020-04-06 14:56:05 +0800

    desmond edited

--- app/src/main/java/com/example/myapplication3/LoginScreen.kt
+++ app/src/main/java/com/example/myapplication3/LoginScreen.kt
@@ -1,4 +1,13 @@
 package com.example.myapplication3
 
-class LoginScreen {
-}
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginScreen : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.login_screen)
+        }
+    }
