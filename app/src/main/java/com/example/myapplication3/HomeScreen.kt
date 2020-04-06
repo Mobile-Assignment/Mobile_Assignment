5c65123dddf449982f546a3e2234d3fbbcf0997e
weemeng111 <weemeng111@gmail.com>
2020-04-06 20:14:41 +0800

    commit

--- app/src/main/java/com/example/myapplication3/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication3/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication3
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.home_screen.*
+import kotlinx.android.synthetic.main.login_screen.*
 
 
 class HomeScreen : AppCompatActivity() {
@@ -10,6 +13,9 @@ class HomeScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
 
-
+        track_trace.setOnClickListener{
+            val intent = Intent(this,PostageCalculator::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/myapplication3/PostageCalculator.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class PostageCalculator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.postage_calculator)
+    }
+}
