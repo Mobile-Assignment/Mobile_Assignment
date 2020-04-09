61a1f80b70f2a46a86141d4d9a6836e26e21f4f0
weilerlim97 <61727235+weilerlim97@users.noreply.github.com>
2020-04-10 00:08:23 +0800

    wl-commit

--- app/src/main/java/com/example/myapplication3/Profile.kt
+++ app/src/main/java/com/example/myapplication3/Profile.kt
@@ -1,4 +1,15 @@
 package com.example.myapplication3
 
-class Profile {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Profile : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.profile_screen, container, false)
+    }
 }
